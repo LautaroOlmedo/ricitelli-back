a131199737448fa8848d89c79018e63513f333ca
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 <lautaroolmedo77@gmail.com>
2026-03-03 00:47:13 -0300

    added server implementation

---
+++ cmd/http/server/server.go
@@ -0,0 +1,54 @@
+package server
+
+import (
+	"context"
+	applicationpb "ricitelli-back/cmd/http/gen/application-service"
+	application_service "ricitelli-back/internal/service/application-service"
+	valueObject "ricitelli-back/internal/value-object"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type Server struct {
+	applicationpb.UnimplementedApplicationServiceServer
+	ApplicationService application_service.Service
+}
+
+func NewServer(applicationService application_service.Service) *Server {
+
+	return &Server{
+		ApplicationService: applicationService,
+	}
+}
+
+func (s *Server) CreateOrder(
+	ctx context.Context,
+	request *applicationpb.CreateOrderRequest,
+) (*applicationpb.CreateOrderResponse, error) {
+
+	if request.CustomerId == "" {
+		return nil, status.Error(codes.InvalidArgument, "invalid param")
+	}
+
+	items := make([]valueObject.SaleOrderItem, 0, len(request.Items))
+
+	for _, item := range request.Items {
+		items = append(items, valueObject.SaleOrderItem{
+			ProductID: item.ProductId,
+			Quantity:  item.Quantity,
+		})
+	}
+
+	err := s.ApplicationService.CreateOrder(
+		ctx,
+		request.CustomerId,
+		items,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &applicationpb.CreateOrderResponse{}, nil
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,47 @@
 package main
+
+import (
+	"log"
+	"net"
+	applicationpb "ricitelli-back/cmd/http/gen/application-service"
+	"ricitelli-back/cmd/http/server"
+	"ricitelli-back/config"
+	repository "ricitelli-back/internal/infraestructure/in-memory"
+	application_service "ricitelli-back/internal/service/application-service"
+	"ricitelli-back/internal/service/product"
+	production_order "ricitelli-back/internal/service/production-order"
+	sale_order "ricitelli-back/internal/service/sale-order"
+
+	"google.golang.org/grpc"
+)
+
+// TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
+// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
+func main() {
+	cfg := config.LoadConfig()
+
+	// repository layer
+	memoryRepo := repository.NewInMemoryRepository()
+
+	// service layer
+	productService := product.NewProductService(&memoryRepo)
+	productionOrderService := production_order.NewProductionOrderService(&memoryRepo)
+	saleOrderService := sale_order.NewSaleOrderService(&memoryRepo)
+
+	applicationService := application_service.NewApplicationService(productService, productionOrderService, saleOrderService)
+
+	grpcServer := grpc.NewServer()
+	svc := server.NewServer(applicationService)
+	applicationpb.RegisterApplicationServiceServer(grpcServer, svc)
+
+	lis, socketErr := net.Listen("tcp", cfg.Port)
+	if socketErr != nil {
+		log.Fatalf("failed to listen: %v", socketErr)
+	}
+
+	log.Printf("gRPC server listening on %s\n", cfg.Port)
+	if serveErr := grpcServer.Serve(lis); serveErr != nil {
+		log.Fatalf("failed to serve: %v", serveErr)
+	}
+
+}
